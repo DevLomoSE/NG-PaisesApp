3719ca896dbd8724bb5114b86601f6574db53a6d
DevLomoSE <jonathancggs@gmail.com>
2021-09-07 20:14:23 -0500

    :bento: Import Components
    importing all the components used by pais module

--- src/app/pais/pais.module.ts
+++ src/app/pais/pais.module.ts
@@ -6,17 +6,23 @@ import { ByPaisComponent } from './pages/by-pais/by-pais.component';
 import { ByRegionComponent } from './pages/by-region/by-region.component';
 import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
 import { ImportsModule } from '../imports/imports.module';
+import { RouterModule } from '@angular/router';
+import { PaisTableComponent } from './components/pais-table/pais-table.component';
+import { PaisInputComponent } from './components/pais-input/pais-input.component';
 
 @NgModule({
   declarations: [
     ByCapitalComponent,
     ByPaisComponent,
     ByRegionComponent,
-    VerPaisComponent
+    VerPaisComponent,
+    PaisTableComponent,
+    PaisInputComponent
   ],
   imports: [
     CommonModule,
-    ImportsModule
+    ImportsModule,
+    RouterModule,
   ],
   exports: [
     ByCapitalComponent,
