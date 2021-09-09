ce54c2ea52015d4e0a68d1e88655ff73825c7a0a
DevLomoSE <jonathancggs@gmail.com>
2021-09-09 08:52:12 -0500

    :lipstick: UI byRegion
    adding some styles to the byRegion page

--- src/app/pais/pages/by-region/by-region.component.ts
+++ src/app/pais/pages/by-region/by-region.component.ts
@@ -9,9 +9,22 @@ export class ByRegionComponent implements OnInit {
 
   private route = '/region';
 
-  constructor() { }
+  regiones = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  regionActiva: string;
+
+  constructor() { 
+    this.regionActiva = '';
+  }
 
   ngOnInit(): void {
+  }
+
+  activarRegion( region: string ): void{
+    this.regionActiva = region;
+  }
+
+  mutateClassCSS( region: string): string{
+    return ( region === this.regionActiva ) ? 'p-button-outlined text-white' : '';
   }
 
 }
