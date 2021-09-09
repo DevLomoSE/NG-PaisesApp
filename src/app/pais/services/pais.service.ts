229417d56a7bedf8c115d673a71bfdeeb3394d1c
DevLomoSE <jonathancggs@gmail.com>
2021-09-09 09:51:28 -0500

    :recycle: HTTP Optimized
    create getMethod to return the reused params for http requests

--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Country } from '../interfaces/pais.interface';
@@ -14,9 +14,15 @@ export class PaisService {
     private http: HttpClient
   ) { }
 
+  get httpParams(): HttpParams{
+    return new HttpParams().set('fields', 'name;capital;flag;population;alpha2Code;alpha3Code');
+  }
+
   buscarPais( name: string): Observable<Country[]>{
     const url = `${this.endpoint}/name/${name}`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url, {
+      params: this.httpParams
+    });
   }
 
   getPaisDetalle( id: string ): Observable<Country>{
@@ -26,11 +32,15 @@ export class PaisService {
 
   buscarCapital( capital: string): Observable<Country[]>{
     const url = `${this.endpoint}/capital/${capital}`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url, {
+      params: this.httpParams
+    });
   }
 
   buscarRegion( region: string): Observable<Country[]>{
     const url = `${this.endpoint}/region/${region}`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url, {
+      params: this.httpParams
+    });
   }
 }
